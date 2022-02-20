45a58a4f89e41948a16ff34d247ede31a1fb9deb
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2022-02-20 23:10:21 +0530

    Added different car images with buttons

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,7 +4,34 @@ import Section from "./Section";
 function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
     </Container>
   );
 }
--- src/app/components/Section.js
+++ src/app/components/Section.js
@@ -1,7 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-function Section() {
-  return <Wrap>Section</Wrap>;
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImage,
+}) {
+  return (
+    <Wrap bgImage={backgroundImage}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          <RightButton>{rightBtnText}</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
 }
 
 export default Section;
@@ -9,5 +29,55 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url("/images/model-s.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const Buttons = styled.div``;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  color: black;
+  opacity: 0.65;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animateDown infinite 1.5s;
 `;
